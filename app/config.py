f5ad8bba6ff95148d5d4efbf0ff6cda4b6dd2ea4
VitorCdSouza <vitorcorddesouza@gmail.com>
2024-10-14 22:29:17 -0300

    probably last commit

--- app/config.py
+++ app/config.py
@@ -1,10 +1,13 @@
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
 
 
 # basic connection configs
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "default_secret_key")
-    JWT_SECRET_KEY = os.environ.get("default_jwt_key")
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "default_jwt_key")
     JWT_ACCESS_TOKEN_EXPIRES = 3600
 
     REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
